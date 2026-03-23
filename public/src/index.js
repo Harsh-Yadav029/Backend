1d6e6ac4d9e51b6ef98996ebae8f9b8f8dd82bf8
Harsh Yadav <harshyadav2027@gmail.com>
2026-03-24 00:35:13 +0530

    utils code

--- public/src/index.js
+++ public/src/index.js
@@ -7,6 +7,15 @@ dotenv.config();
 console.log("ENV:", process.env.MONGODB_URI);
 connectDB()
 
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : $ {process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO DB connection failed ! ! !", err)
+})
+
 
 // import express from "express"
 // const app = express()
